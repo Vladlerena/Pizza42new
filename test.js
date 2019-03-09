d11472642b1b997d38f27c79eb18ed77bc17b54e
Vladimiro Lerena <vllerena@cisco.com>
2019-03-09 11:31:15 -0300

    Update test.js

--- test.js
+++ test.js
@@ -2,9 +2,9 @@ var QSTester = require("auth0-quickstarts-tester");
 
 //Change the values below to match an existing database user on the Test Client
 var testParameters = {
-    url: "http://localhost:3000",
-    user: "email@test.com",
-    password: "testpwd"
+    url: "https://vladlerena.herokuapp.com/",
+    user: "vllerena@cisco.com",
+    password: "Lovecraft1+"
   };
 new QSTester().runOnPath(".", testParameters)
   .then(() => process.exit());
